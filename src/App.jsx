70054328d50cfff4d13a43a3c2803cb27eb91fea
Audrey Feng <audrey.s.feng@gmail.com>
2025-08-27 01:57:21 -0400

    created loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css';
-import { LoadingScreen } from './components/LoadingScreen';
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <>
-      <LoadingScreen />
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
     </>
   );
 }
